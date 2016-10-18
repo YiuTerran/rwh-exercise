b2aa72727f6c420d13db639f5e322d70b2d2cbea
Tairan Yao <tryao@adsquare-tech.com>
2016-10-18 18:13:12 +0800

    add ch6 typeclass to ch5 code

--- ch5/myJSON/src/SimpleJSON.hs
+++ ch5/myJSON/src/SimpleJSON.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE TypeSynonymInstances, OverlappingInstances, FlexibleInstances #-}
 module SimpleJSON where
+
+import Control.Arrow (second)
+
+type JSONError = String
+
+class JSON a where
+    toJValue :: a -> JValue
+    fromJValue :: JValue -> Either JSONError a
 
 data JValue = JString String
             | JNumber Double
@@ -7,6 +16,40 @@ data JValue = JString String
             | JObject [(String, JValue)]
             | JArray [JValue]
               deriving (Eq, Ord, Show)
+
+instance JSON JValue where
+    toJValue = id
+    fromJValue = Right
+
+instance JSON Bool where
+    toJValue = JBool
+    fromJValue (JBool b) = Right b
+    fromJValue _ = Left "not a JSON boolean"
+
+instance JSON String where
+    toJValue               = JString
+
+    fromJValue (JString s) = Right s
+    fromJValue _           = Left "not a JSON string"
+
+doubleToJValue :: (Double -> a) -> JValue -> Either JSONError a
+doubleToJValue f (JNumber v) = Right (f v)
+doubleToJValue _ _ = Left "not a JSON number"
+
+instance JSON Int where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Integer where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Double where
+    toJValue = JNumber
+    fromJValue = doubleToJValue id
+
+listToJValues :: (JSON a) => [a] -> [JValue]
+listToJValues = map toJValue
 
 getString :: JValue -> Maybe String
 getString (JString s) = Just s
