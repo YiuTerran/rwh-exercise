2290692cc9f55f9bdb0b9a67ab6468ec742aa1a9
Tairan Yao <tryao@adsquare-tech.com>
2016-11-10 11:24:06 +0800

    add ch9, 10

---
+++ ch10/PNM.hs
@@ -0,0 +1,47 @@
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+import Data.Char (isSpace)
+
+data Greymap = Greymap {
+    greyWidth :: Int,
+    greyHeight :: Int,
+    grepMax :: Int,
+    grepData :: L.ByteString
+} deriving (Eq)
+
+instance Show Greymap where
+    show (Greymap w h m _) = "Greymap " ++ show w ++ "x" ++ show h ++ " " ++ show m 
+
+parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
+
+matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+matchHeader prefix str
+    | prefix `L8.isPrefixOf` str
+        = Just (L8.dropWhile isSpace (L.drop (L.length prefix) str))
+    | otherwise = Nothing
+getNat :: L.ByteString -> Maybe (Int, L.ByteString)
+getNat s = case L8.readInt s of
+            Nothing -> Nothing
+            Just (num, rest)
+                | num <= 0  -> Nothing
+                | otherwise -> Just (num, L8.dropWhile isSpace rest)
+
+
+getBytes :: Int -> L.ByteString
+         -> Maybe (L.ByteString, L.ByteString)
+getBytes n str = let count = fromIntegral n
+                    both@(prefix,_) = L.splitAt count str
+                 in if L.length prefix < count
+                    then Nothing
+                    else Just both
+
+(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
+Nothing >>? _ = Nothing
+Just v >>? f = f v
+
+
+parse :: Parse a -> L.ByteString -> Either String a
+parse parser initState
+    = case runParse parser (ParseState initState 0) of
+        Left err -> Left err
+        Right (result, _) -> Right result
